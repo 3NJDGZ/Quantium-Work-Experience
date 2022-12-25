47e2c847ab342d0d477a075bc2317f0c6873524b
Alejandro.DGZ <87236751+3NJDGZ@users.noreply.github.com>
2022-12-25 23:45:13 +0000

    CSV File Test

--- main.py
+++ main.py
@@ -3,8 +3,55 @@ import csv
 # Need to take the three csv files as input and then output a singular csv files with the fields:
 # Sales, Date, Region
 
+filtered_data = []
+
 with open('data/daily_sales_data_0.csv', 'r') as file:
     reader = csv.reader(file)
 
     for row in reader:
-        print(row)
+        if row[0] == "pink morsel":
+            price = 0
+            quantity = 0
+            total = 0
+            filtered_row = [row[3], row[4]]
+            price = float(row[1][1:3])
+            quantity = int(row[2])
+            total = price * quantity
+            filtered_row.insert(0, total)
+            filtered_data.append(filtered_row)
+
+with open('data/daily_sales_data_1.csv', 'r') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        if row[0] == "pink morsel":
+            price = 0
+            quantity = 0
+            total = 0
+            filtered_row = [row[3], row[4]]
+            price = float(row[1][1:3])
+            quantity = int(row[2])
+            total = price * quantity
+            filtered_row.insert(0, total)
+            filtered_data.append(filtered_row)
+
+with open('data/daily_sales_data_2.csv', 'r') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        if row[0] == "pink morsel":
+            price = 0
+            quantity = 0
+            total = 0
+            filtered_row = [row[3], row[4]]
+            price = float(row[1][1:3])
+            quantity = int(row[2])
+            total = price * quantity
+            filtered_row.insert(0, total)
+            filtered_data.append(filtered_row)
+
+with open('test.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["sales", "date", "region"])
+    for i in range(len(filtered_data)):
+        writer.writerow(filtered_data[i])
