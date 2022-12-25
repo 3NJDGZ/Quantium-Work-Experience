d060dc05a727b4869f706b6d7c397ba08d0163b8
Alejandro.DGZ <87236751+3NJDGZ@users.noreply.github.com>
2022-12-25 23:22:56 +0000

    Main commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import csv
+# Only interested in Pink Morsels, Sales produced in a given day, the date of the sales and the region
+# Need to take the three csv files as input and then output a singular csv files with the fields:
+# Sales, Date, Region
+
+with open('data/daily_sales_data_0.csv', 'r') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        print(row)
